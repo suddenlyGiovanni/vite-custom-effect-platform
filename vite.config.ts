bda9a9ccf61cd0bf25ed7bea37b9714c6b77e196
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-02-16 21:32:09 +0100

    Refactor project structure and update configurations.
    Moved app-related files to a new "client" directory for better organization. Added missing `.direnv` to excluded folders in project settings. Updated `vite.config.ts` and `react-router.config.ts` with minor fixes and additional configuration options.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import {reactRouter} from '@react-router/dev/vite'
+import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
-import {defineConfig} from 'vite'
-
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-		plugins: [tailwindcss(), reactRouter()],
+	plugins: [tailwindcss(), reactRouter()],
 })
