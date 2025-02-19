b889d7705b334723b0c857fd48f64e081ad30485
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-02-19 02:40:01 +0100

    Refactor middleware import and update dev script options
    Refactored middleware loading to utilize the default export for clarity and type safety. Updated the development script to include `--watch-path` and `--watch-preserve-output` for improved efficiency during file changes. These changes enhance maintainability and development workflow.

--- server/express/development-app.ts
+++ server/express/development-app.ts
@@ -15,10 +15,13 @@ export async function developmentApp(
 
 	app.use(async (req, res, next) => {
 		try {
-			const source = await viteDevServer.ssrLoadModule(
-				'./server/express/app.ts',
-			)
-			return await source['app'](req, res, next)
+			const reactRouterMiddleware = await (
+				viteDevServer.ssrLoadModule('./server/express/app.ts') as Promise<
+					typeof import('./app.ts')
+				>
+			).then((mod) => mod.default)
+
+			return await reactRouterMiddleware(req, res, next)
 		} catch (error) {
 			if (typeof error === 'object' && error instanceof Error) {
 				viteDevServer.ssrFixStacktrace(error)
