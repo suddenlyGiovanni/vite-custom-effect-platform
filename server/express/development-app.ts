609e9d8037fb1fb5ae8368b2f1c5f7d850726754
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-02-19 02:46:31 +0100

    Refactor async middleware to streamline promise handling
    Simplified the logic for loading and applying the React router middleware. Converted the try-catch block to handle errors directly within promise chains for better readability and consistency. This change maintains functionality while improving code clarity.

--- server/express/development-app.ts
+++ server/express/development-app.ts
@@ -13,22 +13,24 @@ export async function developmentApp(
 
 	app.use(viteDevServer.middlewares)
 
-	app.use(async (req, res, next) => {
-		try {
-			const reactRouterMiddleware = await (
-				viteDevServer.ssrLoadModule('./server/express/app.ts') as Promise<
-					typeof import('./app.ts')
-				>
-			).then((mod) => mod.default)
-
-			return await reactRouterMiddleware(req, res, next)
-		} catch (error) {
-			if (typeof error === 'object' && error instanceof Error) {
-				viteDevServer.ssrFixStacktrace(error)
-			}
-			next(error)
-		}
-	})
+	app.use(async (req, res, next) =>
+		(
+			viteDevServer.ssrLoadModule('./server/express/app.ts') as Promise<
+				typeof import('./app.ts')
+			>
+		)
+			.then((mod) => mod.default)
+			.then(
+				async (reactRouterMiddleware) =>
+					await reactRouterMiddleware(req, res, next),
+			)
+			.catch((error) => {
+				if (typeof error === 'object' && error instanceof Error) {
+					viteDevServer.ssrFixStacktrace(error)
+				}
+				next(error)
+			}),
+	)
 
 	return app
 }
