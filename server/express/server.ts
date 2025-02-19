81030caec3a327cac6ce1003e2455cd31ac38777
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-02-19 04:01:32 +0100

    Refactor server initialization with functional pipeline
    Reorganized the server initialization code using `pipe` for improved readability and functional composition. This replaces imperative logic with a more declarative approach, streamlining middleware and environment-specific app setups.

--- server/express/server.ts
+++ server/express/server.ts
@@ -1,26 +1,26 @@
 import compression from 'compression'
+import { pipe } from 'effect'
 import express from 'express'
 import morgan from 'morgan'
-
 import { developmentApp } from './development-app.ts'
 import { productionApp } from './production-app.ts'
 
-const DEVELOPMENT = process.env['NODE_ENV'] === 'development'
 const PORT = Number.parseInt(process.env['PORT'] || '3000')
 
-const app = express()
-
-app.use(compression())
-app.disable('x-powered-by')
-
-if (DEVELOPMENT) {
-	await developmentApp(app)
-} else {
-	await productionApp(app)
-}
-
-app.use(morgan('tiny'))
-
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`)
-})
+pipe(
+	express()
+		.use(compression())
+		.disable('x-powered-by'), //
+	async (app) =>
+		process.env['NODE_ENV'] === 'development'
+			? await developmentApp(app) //
+			: await productionApp(app),
+	(promise) =>
+		promise.then((app) =>
+			app
+				.use(morgan('dev')) //
+				.listen(PORT, () => {
+					console.log(`Server is running on http://localhost:${PORT}`)
+				}),
+		),
+)
