a2d5a16d3b1aae37fdbe7323b15a68d5c21419d3
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-02-19 05:31:25 +0100

    Simplify request handler setup in development app
    Replaced inline request handler logic with a dedicated `reactRouterRequestHandler` function for improved readability and error handling. This change ensures better stack trace fixing and maintains cleaner code structure.

--- server/express/development-app.ts
+++ server/express/development-app.ts
@@ -4,7 +4,7 @@ const viteDevServer = await import('vite').then((vite) =>
 	vite.createServer({ server: { middlewareMode: true } }),
 )
 
-const reactRouterRequestHandler = await (
+const expressRequestHandler = await (
 	viteDevServer.ssrLoadModule('./server/express/app.ts') as Promise<
 		typeof import('./app.ts')
 	>
@@ -12,21 +12,27 @@ const reactRouterRequestHandler = await (
 	createExpressRequestHandlerAdapter(),
 )
 
+const reactRouterRequestHandler: typeof expressRequestHandler = async (
+	req,
+	res,
+	next,
+) => {
+	try {
+		return await expressRequestHandler(req, res, next)
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			viteDevServer.ssrFixStacktrace(error)
+		}
+		next(error)
+	}
+}
+
 export async function developmentApp(
 	app: express.Express,
 ): Promise<express.Express> {
 	console.log('Starting development server')
 
-	return app
-		.use(viteDevServer.middlewares) //
-		.use(async (req, res, next) => {
-			try {
-				return await reactRouterRequestHandler(req, res, next)
-			} catch (error) {
-				if (typeof error === 'object' && error instanceof Error) {
-					viteDevServer.ssrFixStacktrace(error)
-				}
-				next(error)
-			}
-		})
+	return app //
+		.use(viteDevServer.middlewares)
+		.use(reactRouterRequestHandler)
 }
