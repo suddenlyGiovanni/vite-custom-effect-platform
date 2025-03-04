2c7c285e4b3db6edce14f27c449220ca0a05ee6a
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-03-05 00:53:17 +0100

    Refactor handler to use `Effect.gen` for improved readability
    Replaced nested `Effect.andThen` calls with `Effect.gen` to streamline asynchronous operations and improve code maintainability. This change simplifies the handler logic and makes it easier to follow.

--- server/effect-platform/handler/handler.ts
+++ server/effect-platform/handler/handler.ts
@@ -8,17 +8,15 @@ export const handler: Effect.Effect<
 	HttpServerResponse.HttpServerResponse,
 	never,
 	HttpServerRequest.HttpServerRequest | ConfigService
-> = ConfigService.pipe(
-	Effect.andThen((config) => config.NODE_ENV),
-	Effect.andThen((NODE_ENV) =>
-		createHttpHandler({
-			build() {
-				// @ts-expect-error - This is a dynamic import of build stuff
-				return import('virtual:react-router/server-build') // TODO: should change this api to an effect!
-			},
-			getLoadContext: () =>
-				Effect.succeed({ VALUE_FROM_EXPRESS: 'Hello from Express' }),
-			mode: NODE_ENV,
-		}),
-	),
-)
+> = Effect.gen(function* () {
+	const { NODE_ENV } = yield* ConfigService
+	return yield* createHttpHandler({
+		build() {
+			// @ts-expect-error - This is a dynamic import of build stuff
+			return import('virtual:react-router/server-build') // TODO: should change this api to an effect!
+		},
+		getLoadContext: () =>
+			Effect.succeed({ VALUE_FROM_EXPRESS: 'Hello from Express' }),
+		mode: NODE_ENV,
+	})
+})
